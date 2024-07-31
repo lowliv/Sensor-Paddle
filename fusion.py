6e731a9c43a39668e622e7ad8909d914aac41c1b
Cameron MacMillan <camacmi@proton.me>
2024-07-30 23:39:08 -0400

    added kalman filter

--- fusion.py
+++ fusion.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as pyplot
 import numpy
 import sys
 import os
+from pykalman import KalmanFilter
 
 def datato_listarray(matrix,length):    
     for n in range(length):
@@ -98,6 +99,8 @@ ahrs.settings = imufusion.Settings(imufusion.CONVENTION_NWU,  # convention
 # Process sensor data
 delta_time = numpy.diff(timestamp, prepend=timestamp[0])
 
+
+earth = numpy.empty((len(timestamp), 3))
 euler = numpy.empty((len(timestamp), 3))
 internal_states = numpy.empty((len(timestamp), 6))
 flags = numpy.empty((len(timestamp), 4))
@@ -108,7 +111,9 @@ for index in range(len(timestamp)):
     ahrs.update(gyroscope[index], accelerometer[index], magnetometer[index], delta_time[index])
 
     euler[index] = ahrs.quaternion.to_euler()
-
+    earth[index] = ahrs.earth_acceleration
+    print(earth[index])
+    
     ahrs_internal_states = ahrs.internal_states
     internal_states[index] = numpy.array([ahrs_internal_states.acceleration_error,
                                           ahrs_internal_states.accelerometer_ignored,
@@ -178,4 +183,44 @@ axes[9].legend()
 
 plot_bool(axes[10], timestamp, flags[:, 3], "Magnetic recovery")
 
-pyplot.show(block="no_block" not in sys.argv)  # don't block when script run by CI
+import numpy as np
+import pylab as pl
+
+from pykalman import KalmanFilter
+
+rnd = np.random.RandomState(0)
+
+# generate a noisy sine wave to act as our fake observations
+n_timesteps = length
+print(n_timesteps)
+x = np.linspace(0, 3 * np.pi, n_timesteps)
+observations = 20 * (np.sin(x) + 0.5 * rnd.randn(n_timesteps))
+observations = [value[0] for value in earth] 
+print(observations)
+print(len(observations))
+# create a Kalman Filter by hinting at the size of the state and observation
+# space.  If you already have good guesses for the initial parameters, put them
+# in here.  The Kalman Filter will try to learn the values of all variables.
+kf = KalmanFilter(transition_matrices=np.array([[1, 1], [0, 1]]),
+                  transition_covariance=0.01 * np.eye(2))
+
+# You can use the Kalman Filter immediately without fitting, but its estimates
+# may not be as good as if you fit first.
+states_pred = kf.em(observations).smooth(observations)[0]
+print('fitted model: {0}'.format(kf))
+
+# Plot lines for the observations without noise, the estimated position of the
+# target before fitting, and the estimated position after fitting.
+pl.figure(figsize=(16, 6))
+obs_scatter = pl.scatter(x, observations, marker='x', color='b',
+                         label='observations')
+position_line = pl.plot(x, states_pred[:, 0],
+                        linestyle='-', marker='o', color='r',
+                        label='position est.')
+velocity_line = pl.plot(x, states_pred[:, 1],
+                        linestyle='-', marker='o', color='g',
+                        label='velocity est.')
+pl.legend(loc='lower right')
+pl.xlim(xmin=0, xmax=x.max())
+pl.xlabel('time')
+pl.show()
